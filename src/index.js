3b923013be5730ba6deaec3b1683f29887ab3fac
abdidaniel <abdidaniel2000@gmail.com>
2024-01-08 17:52:40 -0500

    arreglo cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 require('./db');
+const cors = require('cors')
 const app = require('./app');
 const port = process.env.PORT || 5000;
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-  });
+app.use(cors());
 
 app.listen(port, () => {
     console.log("Server running on port", port);
